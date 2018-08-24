42fcde33760dae8e850985420e1de2e48d3fbbcb
q25979 <277161542@qq.com>
2018-08-24 22:14:59 +0800

    前端页面完成

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,7 +3,20 @@ namespace app\index\controller;
 
 class Index extends Validate
 {
+    // 首页
     public function index()
+    {
+        return $this->fetch();
+    }
+
+    // 类别
+    public function category()
+    {
+        return $this->fetch();
+    }
+
+    // 视频播放
+    public function play()
     {
         return $this->fetch();
     }
