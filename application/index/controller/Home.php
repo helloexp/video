5b84479abc3782f6d2bdb9cc5ef9d969f18494d4
q25979 <277161542@qq.com>
2018-08-26 15:22:31 +0800

    前端基本完成

--- application/index/controller/Home.php
+++ application/index/controller/Home.php
@@ -8,7 +8,7 @@ use think\facade\Request;
 
 class Home extends Validate
 {
-    //首页分类
+    // 首页分类
     public function classify()
     {
         $result = Type::field('create_time, update_time, deleted_time', true)
@@ -17,7 +17,7 @@ class Home extends Validate
         return json($result);
     }
 
-    //首页搜索
+    // 首页搜索
     public function seach()
     {
         $data = Request::get();
@@ -28,14 +28,14 @@ class Home extends Validate
         return json($result);
     }
 
-    //特别推荐
+    // 特别推荐
     public function recommend()
     {
         $result = Recommend::linkVideo()->select();
         return json($result);
     }
 
-    //最新视频
+    // 最新视频
     public function newVideo()
     {
         $result['count'] = Video::count();
@@ -47,7 +47,7 @@ class Home extends Validate
         return json($result);
     }
 
-    //热门电影
+    // 热门电影
     public function hotVideo()
     {
         $result['data'] =Video::taskout()
@@ -57,14 +57,21 @@ class Home extends Validate
         return json($result);
     }
 
-    //分类电影
+    // 分类电影
     public function type()
     {
         $data = Request::get();
-        $map['vt.id'] = $data['id'];
-        $result['data'] = Type::linkVideo()
-            ->where($map)
+        $map['type'] = $data['id'];
+
+        $result['code'] = 0;
+        $result['msg']  = "数据获取成功";
+        $result['count'] = Video::where($map)->count();
+        $result['data'] = Video::where($map)
+            ->page($data['page'], $data['limit'])
+            ->field('type, step_on, update_time, deleted_time', true)
+            ->order('create_time', 'desc')
             ->select();
+
         return json($result);
     }
 }
